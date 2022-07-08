dd8d21c81128ed3cf4e3aa7da894d69f069d3011
Akash Verma <vermaakash60584@gmail.com>
2022-07-08 15:26:20 +0530

    Second Commit

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Assignment.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Assignment.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Employee> employeeList = new List<Employee>
+            {
+                new Employee {EId=1,EName="Akash Verma",EDesignation="Progammer Analyst",EDOJ=DateTime.Parse("2022-02-12")},
+                new Employee {EId=2,EName="Owen Norris",EDesignation="Consultant",EDOJ=DateTime.Parse("2021-04-15")},
+                new Employee {EId=3,EName="Lara Carrillo",EDesignation="Tester",EDOJ=DateTime.Parse("2020-10-29")},
+                new Employee {EId=4,EName="Nora Mcclain",EDesignation="System Engineer",EDOJ=DateTime.Parse("2015-12-10")},
+                new Employee {EId=5,EName="Dan Hall",EDesignation="Trainee Engineer",EDOJ=DateTime.Parse("2021-07-06")}
+            };
+
+            return View(employeeList);
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment.Models
+{
+    public class Employee
+    {
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public string EDesignation { get; set; }
+        public DateTime EDOJ { get; set; }
+    }
+}
